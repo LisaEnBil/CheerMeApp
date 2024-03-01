bcec993daa29f67560b1e26be752a7f11bf361a2
Lisa <lisaenbil1@gmail.com>
2024-03-01 20:20:37 +0100

    Initial Commit

---
+++ CheerApp/CheerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheerAppApp.swift
+//  CheerApp
+//
+//  Created by Lisa Gillfrost on 2024-03-01.
+//
+
+import SwiftUI
+
+@main
+struct CheerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
