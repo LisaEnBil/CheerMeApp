40a6e5cb65bae3325c05e4b139185d8580ce4873
Lisa <lisaenbil1@gmail.com>
2024-03-03 21:16:28 +0100

    remove duplicate "App" in app name

---
+++ CheerApp/CatView.swift
@@ -0,0 +1,18 @@
+//
+//  CatView.swift
+//  CheerApp
+//
+//  Created by Lisa Gillfrost on 2024-03-03.
+//
+
+import SwiftUI
+
+struct CatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CatView()
+}
--- CheerApp/CheerApp.swift
+++ CheerApp/CheerApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct CheerAppApp: App {
+struct CheerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
