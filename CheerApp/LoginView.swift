643b2c3cc4e3f8ab76855be798325cb144da01b5
Lisa Gillfrost <smartalisa@hotmail.com>
2024-04-06 12:36:58 +0200

    fix registrering function

--- CheerApp/LoginView.swift
+++ CheerApp/LoginView.swift
@@ -9,35 +9,70 @@ import SwiftUI
 import Firebase
 
 struct LoginView: View {
-        
+    
     @State var email = ""
     @State var password = ""
+    @State var passwordCopy = ""
+    @State var isMatching = true
+    @State var isRegistrering = false
+    @State var text = ""
+    
     @ObservedObject var userAuth = UserAuthentication()
+    
     
     var body: some View {
         VStack {
             
+            
             Text(userAuth.isIncorrect ? "Email or Password is incorrect" : "")
-          
+            
+            Text(isMatching == false ? "Passwords doesn't match" : "")
+            
             TextField("Email", text: $email )
             
             TextField("Password", text: $password ).textCase(.lowercase)
             
-            Button(action: {
-                userAuth.login(email: email, password: password)
-            }, label: {
-            Text("Login")
-            })
+            if isRegistrering == true  {
+                TextField("Password again", text: $passwordCopy ).textCase(.lowercase)
+            }
+            if isRegistrering == true {
+                Button(action: {
+                    if password == passwordCopy {
+                        isMatching = true
+                        isRegistrering = false
+                        userAuth.register(email: email, password: password)
+                    }else {
+                        isMatching = false
+                    }
+                    
+                }, label: {
+                    Text("Submit")
+                    
+                })
+            } else {
+                
+                Button(action: {
+                    userAuth.login(email: email, password: password)
+                }, label: {
+                    Text("Login")
+                })
+                
+                Button(action: {
+                    if isRegistrering == false {
+                        isRegistrering = true
+                    }
+                }, label: {
+                    Text("Register")
+                    
+                })
+            }
+          
+           
             
-            Button(action: {
-                userAuth.register(email: email, password: password)
-            }, label: {
-            Text("Register")
-   
-            })
+            
         }
     }
-  
+    
 }
 
 #Preview {
