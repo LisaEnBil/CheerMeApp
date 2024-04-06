d831ac250401beb1a1a345cd525683b21fc0cc24
Lisa Gillfrost <smartalisa@hotmail.com>
2024-04-06 14:35:57 +0200

    add some styling to login view

--- CheerApp/LoginView.swift
+++ CheerApp/LoginView.swift
@@ -9,73 +9,73 @@ import SwiftUI
 import Firebase
 
 struct LoginView: View {
+ 
+ @State var email = ""
+ @State var password = ""
+ @State var passwordCopy = ""
+ @State var isMatching = true
+ @State var isRegistrering = false
+ @State var text = ""
+ 
+ @ObservedObject var userAuth = UserAuthentication()
+ 
+ 
+ var body: some View {
+  VStack {
+   Text(userAuth.isIncorrect ? "Email or Password is incorrect" : "").foregroundColor(Color(red:1, green:0.57684861730000003, blue:0.64454441770000004))
+   
+   Text(isMatching == false ? "Passwords doesn't match" : "").foregroundColor(Color(red:1, green:0.57684861730000003, blue:0.64454441770000004))
+   
+   TextField("Email", text: $email ).background(.white).padding()
+   
+   TextField("Password", text: $password ).background(.white).padding()
+   
+   if isRegistrering == true  {
+    TextField("Password again", text: $passwordCopy ).textCase(.lowercase).background(.white).padding()
+   }
+   if isRegistrering == true {
+    Button(action: {
+     if password == passwordCopy {
+      isMatching = true
+      isRegistrering = false
+      userAuth.register(email: email, password: password)
+     }else {
+      isMatching = false
+     }
+    }, label: {
+     Text("Submit")
+    }).padding()
     
-    @State var email = ""
-    @State var password = ""
-    @State var passwordCopy = ""
-    @State var isMatching = true
-    @State var isRegistrering = false
-    @State var text = ""
+    Button(action: {
+     isRegistrering = false
+    }, label: {
+     Text("Close")
+    }).padding()
     
-    @ObservedObject var userAuth = UserAuthentication()
+   } else {
+    Button(action: {
+     userAuth.login(email: email, password: password)
+    }, label: {
+     Text("Login")
+    }).padding()
     
-    
-    var body: some View {
-        VStack {
-            
-            
-            Text(userAuth.isIncorrect ? "Email or Password is incorrect" : "")
-            
-            Text(isMatching == false ? "Passwords doesn't match" : "")
-            
-            TextField("Email", text: $email )
-            
-            TextField("Password", text: $password ).textCase(.lowercase)
-            
-            if isRegistrering == true  {
-                TextField("Password again", text: $passwordCopy ).textCase(.lowercase)
-            }
-            if isRegistrering == true {
-                Button(action: {
-                    if password == passwordCopy {
-                        isMatching = true
-                        isRegistrering = false
-                        userAuth.register(email: email, password: password)
-                    }else {
-                        isMatching = false
-                    }
-                    
-                }, label: {
-                    Text("Submit")
-                    
-                })
-            } else {
-                
-                Button(action: {
-                    userAuth.login(email: email, password: password)
-                }, label: {
-                    Text("Login")
-                })
-                
-                Button(action: {
-                    if isRegistrering == false {
-                        isRegistrering = true
-                    }
-                }, label: {
-                    Text("Register")
-                    
-                })
-            }
-          
-           
-            
-            
-        }
-    }
-    
+    Button(action: {
+     if isRegistrering == false {
+      isRegistrering = true
+     }
+    }, label: {
+     Text("Register")
+     
+    }).padding()
+   }
+  }.frame(maxHeight: .infinity).background(Color(white: 0.258))
+ }
+ 
 }
 
 #Preview {
-    LoginView()
+ LoginView()
 }
 
+
+//Color(red:1, green:0.57684861730000003, blue:0.64454441770000004)
