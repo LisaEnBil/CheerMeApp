24881608c4224878b4165f2b26cd2238a461a4bd
Lisa Gillfrost <smartalisa@hotmail.com>
2024-04-13 10:49:09 +0200

    move function to class

--- CheerApp/CatHelpers.swift
+++ CheerApp/CatHelpers.swift
@@ -18,100 +18,64 @@ struct CatModel: Identifiable {
     var audio: URL
 }
 
-
 class CatHelpers: ObservableObject {
     
     @Published var image: UIImage?
     @Published var cats: [CatModel] = []
-    //@Published var cat: CatModel = CatModel(name: "", audio: "")
     
-//    func saveCat(addCat: CatModel) {
-//        
-//        
-//        if addCat.name == "" {
-//            return
-//        }
-//        var ref : DatabaseReference!
-//        
-//        let storage = Storage.storage()
-//        let storageRef = storage.reference()
-//        
-//        let imageData: Data? = image?.jpegData(compressionQuality: 0)
-//        
-//      
-//
-//    let imagesRef = storageRef.child("images/" + addCat.name + ".png" )
-//        
-//        imagesRef.putData((imageData)!, metadata: nil) { (metadata, error) in
-//          guard let metadata = metadata else {
-//            // Uh-oh, an error occurred!
-//            return
-//          }
-//          // Metadata contains file metadata such as size, content-type.
-//          let size = metadata.size
-//          // You can also access to download URL after upload.
-//            imagesRef.downloadURL { (url, error) in
-//            guard let downloadURL = url else {
-//              // Uh-oh, an error occurred!
-//              return
-//            }
-//          }
-//        }
-//        
-//        ref = Database.database().reference()
-//        
-//        var cat = [String: Any]()
-//        
-//        cat["name"] = addCat.name
-//        let uid = Auth.auth().currentUser!.uid
-//        ref.child("catlist").child(uid).childByAutoId().setValue(cat)
-//        
-//        let newCat = CatModel(name: addCat.name, audio: "0981")
-//        self.addCat(newCat)
-//
-//    }
+    func loadStoredCats(dbRef: String) {
+        
+        var ref : DatabaseReference!
+        ref = Database.database().reference()
+        
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        
+        let uid = Auth.auth().currentUser!.uid
+        
+        let dbPrefix = dbRef == "library_cats" ? ref.child(dbRef) : ref.child(dbRef).child(uid)
+        
+        dbPrefix.getData(completion: {error, snapshot in
+            
+            for todochild in snapshot!.children {
+                
+                let childsnap = todochild as! DataSnapshot
+                
+                if let theCat = childsnap.value as? [String: Any] {
+                    
+                    
+                    let name = theCat["name"] as! String
+                    let imageRef = dbRef == "library_cats" ? storageRef.child("library_cats/" + childsnap.key + ".jpg") 
+                    : storageRef.child("users/" + uid + "/" + name + "/" + name + ".png")
+
+                    let audioRef = dbRef == "library_cats" ? storageRef.child("library_cats/" + childsnap.key + ".wav") 
+                    : storage.reference(withPath: "users/" + uid + "/" + name + "/"  + name + ".wav")
+                    
+                    Task {
+                        do {
+                            let imageData = try await imageRef.data(maxSize: 1 * 1024 * 1024)
+                            let imagePlace = UIImage(data: imageData)!
+                            let audioData = try await audioRef.data(maxSize: 100 * 1024 * 1024)
+                            
+                            let audioURL = dbRef == "library_cats" ? URL(fileURLWithPath: NSTemporaryDirectory() + "\(childsnap.key).wav") : URL(fileURLWithPath: NSTemporaryDirectory() + "\(name).wav")
+                            try audioData.write(to: audioURL)
+                            let catModel = CatModel(name: name, image: imagePlace, audio: audioURL)
+                            self.cats.append(catModel)
+                        } catch {
+                            print("Error fetching data: \(error)")
+                        }
+                    }
+                }
+            }
+        })
+    }
+
     
     
     
     func addCat(_ cat: CatModel) {
         cats.append(cat)
     }
-    
-    
-//    func loadtodo() {
-//        
-//        var ref : DatabaseReference!
-//        ref = Database.database().reference()
-//        let uid = Auth.auth().currentUser!.uid
-//        
-//        ref.child("catlist").child(uid).getData(completion: {error, snapshot in
-//            var tempList = [CatModel]()
-//            
-//            for todochild in snapshot!.children {
-//                
-//                let childsnap = todochild as! DataSnapshot
-//                
-//                var cat = [String: Any]()
-//                
-//                if let thetodo = childsnap.value as? [String: Any] {
-//                    
-//                    var tempCat = CatModel(name: "", )
-//                    tempCat.name =  cat["name"] as! String
-//                    //tempCat.audio = cat["audio"] as? String
-//                    
-//                    tempList.append(tempCat)
-//                    
-//                }
-//            }
-//            
-//            
-//          
-//            
-//            /*
-//             if let thetodo = snapshot?.value as? String {
-//             addtodo = thetodo
-//             }*/
-//        })
-//    }
+
     
 }
--- CheerApp/ContentView.swift
+++ CheerApp/ContentView.swift
@@ -11,34 +11,32 @@ import PhotosUI
 import Firebase
 import FirebaseStorage
 
-
-
-
 struct ContentView: View {
     
     @State private var showAddCatModal = false
     @State private var showSettingsModal = false
     @State var image: UIImage?
-    @State var cats : [CatModel] = []
+    
+    @ObservedObject var catHelpers = CatHelpers()
     
     var image1: UIImage?
-
+    
     var body: some View {
         NavigationStack {
             
             Button("Add cat") {
                 showAddCatModal = true
             } .sheet(isPresented: $showAddCatModal) {
-                AddCatView(showModal: $showAddCatModal, image: $image, name: "", cats: $cats)
+                AddCatView(showModal: $showAddCatModal, image: $image, name: "", cats: $catHelpers.cats)
             }
             
-            List(cats) { cat in
+            List(catHelpers.cats) { cat in
                 NavigationLink {
                     CatView(model: cat)
                     
                     
                 } label: {
-                 
+                    
                     VStack(alignment: .center) {
                         Image(uiImage: cat.image)
                             .resizable()
@@ -63,131 +61,9 @@ struct ContentView: View {
                 
             }
         }.onAppear(){
-//            loadtodo()
-//           loadLibraryCats()
-            loadStoredCats(dbRef: "library_cats" )
-            loadStoredCats(dbRef: "user_cat_list" )
+            catHelpers.loadStoredCats(dbRef: "library_cats" )
+            catHelpers.loadStoredCats(dbRef: "user_cat_list" )
         }
-    }
-    
-    func loadLibraryCats() {
-        var ref: DatabaseReference!
-        ref = Database.database().reference()
-
-        let storage = Storage.storage()
-        let storageRef = storage.reference()
-
-        ref.child("library_cats").getData(completion: { error, snapshot in
-            for todochild in snapshot!.children {
-                let childsnap = todochild as! DataSnapshot
-                if let theCat = childsnap.value as? [String: Any] {
-                    let imageRef = storageRef.child("library_cats/" + childsnap.key + ".jpg")
-                    let audioRef = storageRef.child("library_cats/" + childsnap.key + ".wav")
-
-                    Task {
-                        do {
-                            let imageData = try await imageRef.data(maxSize: 1 * 1024 * 1024)
-                            let imagePlace = UIImage(data: imageData)!
-
-                            let audioData = try await audioRef.data(maxSize: 100 * 1024 * 1024) // Increase the max size if needed
-                            let audioURL = URL(fileURLWithPath: NSTemporaryDirectory() + "\(childsnap.key).wav")
-                            try audioData.write(to: audioURL)
-
-                            let catModel = CatModel(name: theCat["name"] as! String, image: imagePlace, audio: audioURL)
-                            cats.append(catModel)
-                        } catch {
-                            print("Error fetching data: \(error)")
-                        }
-                    }
-                }
-            }
-        })
-    }
-    
-    func loadtodo() {
-        
-        var ref : DatabaseReference!
-        ref = Database.database().reference()
-        
-        let storage = Storage.storage()
-        let storageRef = storage.reference()
-        
-        let uid = Auth.auth().currentUser!.uid
-        
-        ref.child("user_cat_list").child(uid).getData(completion: {error, snapshot in
-            
-            for todochild in snapshot!.children {
-                
-                let childsnap = todochild as! DataSnapshot
-                
-                if let theCat = childsnap.value as? [String: Any] {
-                    
-                    let name = theCat["name"] as! String
-                    let imageRef = storageRef.child("users/" + uid + "/" + name + "/" + name + ".png")
-                    let audioRef = storage.reference(withPath: "users/" + uid + "/" + name + "/" + name + ".wav")
-                    Task {
-                        do {
-                            let imageData = try await imageRef.data(maxSize: 1 * 1024 * 1024)
-                            let imagePlace = UIImage(data: imageData)!
-                            
-                            
-                            let audioData = try await audioRef.data(maxSize: 100 * 1024 * 1024)
-                            let audioURL = URL(fileURLWithPath: NSTemporaryDirectory() + "\(name).wav")
-                            try audioData.write(to: audioURL)
-                            let catModel = CatModel(name: name, image: imagePlace, audio: audioURL)
-                            cats.append(catModel)
-                        } catch {
-                            print("Error fetching data: \(error)")
-                        }
-                    }
-                }
-            }
-        })
-    }
-    
-    func loadStoredCats(dbRef: String) {
-        
-        var ref : DatabaseReference!
-        ref = Database.database().reference()
-        
-        let storage = Storage.storage()
-        let storageRef = storage.reference()
-        
-        let uid = Auth.auth().currentUser!.uid
-        
-        let g = dbRef == "library_cats" ? ref.child(dbRef) : ref.child(dbRef).child(uid)
-        
-        g.getData(completion: {error, snapshot in
-            
-            for todochild in snapshot!.children {
-                
-                let childsnap = todochild as! DataSnapshot
-                
-                if let theCat = childsnap.value as? [String: Any] {
-                    
-                    let name = theCat["name"] as! String
-
-                    let imageRef = dbRef == "library_cats" ? storageRef.child("library_cats/" + childsnap.key + ".jpg") : storageRef.child("users/" + uid + "/" + name + "/" + name + ".png")
-                    
-                    let audioRef = dbRef == "library_cats" ? storageRef.child("library_cats/" + childsnap.key + ".wav") : storage.reference(withPath: "users/" + uid + "/" + name + "/" + name + ".wav")
-                    
-                    Task {
-                        do {
-                            let imageData = try await imageRef.data(maxSize: 1 * 1024 * 1024)
-                            let imagePlace = UIImage(data: imageData)!
-                            let audioData = try await audioRef.data(maxSize: 100 * 1024 * 1024)
-                            
-                            let audioURL = dbRef == "library_cats" ? URL(fileURLWithPath: NSTemporaryDirectory() + "\(childsnap.key).wav")  :   URL(fileURLWithPath: NSTemporaryDirectory() + "\(name).wav")
-                            try audioData.write(to: audioURL)
-                            let catModel = CatModel(name: theCat["name"] as! String, image: imagePlace, audio: audioURL)
-                            cats.append(catModel)
-                        } catch {
-                            print("Error fetching data: \(error)")
-                        }
-                    }
-                }
-            }
-        })
     }
 }
 
