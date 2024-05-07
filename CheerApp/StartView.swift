de4b6a9d40641ca753f3096b583fd8f9a50a061c
Lisa Gillfrost <smartalisa@hotmail.com>
2024-05-07 13:02:56 +0200

    add splashscreen

--- CheerApp/StartView.swift
+++ CheerApp/StartView.swift
@@ -11,34 +11,56 @@ import Firebase
 struct StartView: View {
     
     @State var isLoggedIn = false
+    @State var showSplash: Bool = false
     
     var body: some View {
         
-        VStack {
-            
-            if isLoggedIn {
-                ContentView()
-            }else {
-                LoginView()
+        ZStack {
+            if self.showSplash {
+                VStack {
+                    
+                    if isLoggedIn {
+                        ContentView()
+                    }else {
+                        LoginView()
+                    }
+                }
+            } else {
+                
+                Color(red: 237/255, green: 119/255, blue: 123/255)
+                    .ignoresSafeArea()
+                Image(uiImage: UIImage(named: "AppIcon")!)
+                 .resizable()
+                 .aspectRatio(contentMode: .fit)
+                 .frame(width: 150.0, height: 150.0)
+                 .cornerRadius(5.0)
+              
+               
             }
-        }
-        .onAppear(){
-            Auth.auth().addStateDidChangeListener {
-                auth, user in
-                                
-                if Auth.auth().currentUser == nil {
-                    isLoggedIn = false
-                }else {
-                    isLoggedIn = true
-                    print(Auth.auth().currentUser?.uid as Any)
+            
+        }.onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                withAnimation {
+                    self.showSplash = true
+                }
+                
+                
+                Auth.auth().addStateDidChangeListener {
+                    auth, user in
+                                    
+                    if Auth.auth().currentUser == nil {
+                        isLoggedIn = false
+                    }else {
+                        isLoggedIn = true
+                        print(Auth.auth().currentUser?.uid as Any)
+                    }
                 }
             }
-            
         }
-        .background(concrete)
     }
 }
 
 #Preview {
     StartView()
 }
+
