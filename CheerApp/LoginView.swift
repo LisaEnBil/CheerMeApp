3a1b2ea86fe1751237a0f1a451cebcc7c780549b
Lisa Gillfrost <smartalisa@hotmail.com>
2024-05-07 10:50:03 +0200

    show error handling for authentication functions

--- CheerApp/LoginView.swift
+++ CheerApp/LoginView.swift
@@ -16,20 +16,19 @@ struct LoginView: View {
  
  var body: some View {
   VStack {
-
-    if isLoggingIn  {
-     Login(isLoggingIn: $isLoggingIn)
-     
-    } else if isRegistering{
-     SignUp(isRegistering: $isRegistering)
-     
-    } else if isResettingPassword{
-     ResetPassword(isResettingPassword: $isResettingPassword)
-    }
-    else {
-     FirstPageButtonView(isRegistering: $isRegistering, isLoggingIn: $isLoggingIn, isResettingPassword: $isResettingPassword)
+   
+   if isLoggingIn  {
+    Login(isLoggingIn: $isLoggingIn)
+    
+   } else if isRegistering{
+    SignUp(isRegistering: $isRegistering)
+    
+   } else if isResettingPassword{
+    ResetPassword(isResettingPassword: $isResettingPassword)
+   } else {
+    FirstPageButtonView(isRegistering: $isRegistering, isLoggingIn: $isLoggingIn, isResettingPassword: $isResettingPassword)
    }
-
+   
   }.frame(maxWidth: .infinity).frame( maxHeight: .infinity).background(concrete)
  }
 }
@@ -50,7 +49,7 @@ struct FirstPageButtonView: View {
    
    VStack {
     Spacer()
-   
+    
     Image(uiImage: UIImage(named: "AppIcon")!)
      .resizable()
      .aspectRatio(contentMode: .fit)
@@ -84,15 +83,13 @@ struct FirstPageButtonView: View {
     
     Button(action: {
      isResettingPassword = true
- 
+     
     }, label: {
      Text("Reset password")
     }).modifier(ButtonStyle())
     
-
     Spacer()
    }
-
   }
  }
 }
@@ -127,6 +124,7 @@ struct SignUp: View {
  @State var isMatching = true
  @Binding var isRegistering: Bool
  @ObservedObject var userAuth = UserAuthentication()
+ @State private var showError = false
  
  var body: some View {
   
@@ -134,41 +132,54 @@ struct SignUp: View {
    BackButton(Boolean: $isRegistering)
    
    Spacer()
+   Text(isMatching == false ? "Passwords doesn't match" : "").foregroundColor(.red)
    
-   VStack {
-    
-    Text(isMatching == false ? "Passwords doesn't match" : "").foregroundColor(Color(red:1, green:0.57684861730000003, blue:0.64454441770000004))
-    
-    VStack {
-     Text("Email:").modifier(TextFieldLabelStyle())
-     TextField("Email", text: $email )
-      .modifier(TextFieldStyle())
-    }.padding()
-   
-    VStack {
-     Text("Password:").modifier(TextFieldLabelStyle())
-     SecureField("Password", text: $password )
-      .modifier(TextFieldStyle())
-    }.padding()
-   
-    VStack {
-     Text("Password again:").modifier(TextFieldLabelStyle())
-     SecureField("Password again", text: $passwordCopy )
-      .modifier(TextFieldStyle())
-    }.padding()
-    
-    Button(action: {
-     if password == passwordCopy {
-      isMatching = true
-      isRegistering = false
-      userAuth.register(email: email, password: password)
-     }else {
-      isMatching = false
-     }
-    }, label: {
-     Text("Submit")
-    }).modifier(ButtonStyle())
+   if showError {
+    Text("Registration failed, incorrect email format.")
+     .foregroundColor(.red)
+     .padding()
    }
+   
+   Text("Email:").modifier(TextFieldLabelStyle())
+   TextField("Email", text: $email )
+    .modifier(EmailFieldStyle())
+    .modifier(TextFieldStyle())
+    .padding([.bottom], 10)
+
+   Text("Password:").modifier(TextFieldLabelStyle())
+   SecureField("Password", text: $password )
+    .disableAutocorrection(true)
+    .modifier(TextFieldStyle())
+    .padding([.bottom], 10)
+   
+   Text("Password again:").modifier(TextFieldLabelStyle())
+   SecureField("Password again", text: $passwordCopy )
+    .disableAutocorrection(true)
+    .modifier(TextFieldStyle())
+    .padding([.bottom], 10)
+   
+   Button(action: {
+    
+    if password == passwordCopy {
+     isMatching = true
+ 
+     Task {
+      do {
+       try await userAuth.registerUser(email: email, password: password)
+       isRegistering = false
+       print("Registration and email verification successful")
+      } catch {
+       isRegistering = true
+       showError = true
+      }
+     }
+    }else {
+     isMatching = false
+    }
+   }, label: {
+    Text("Submit")
+   }).modifier(ButtonStyle())
+   
    Spacer()
   }
  }
@@ -185,32 +196,29 @@ struct Login: View {
   
   VStack {
    BackButton(Boolean: $isLoggingIn)
-   
    Spacer()
+   Text(userAuth.isIncorrect ? "Email or Password is incorrect" : "").foregroundColor(.red)
    
-   Text(userAuth.isIncorrect ? "Email or Password is incorrect" : "").foregroundColor(Color(red:1, green:0.57684861730000003, blue:0.64454441770000004))
+   Text("Email:").modifier(TextFieldLabelStyle())
+   TextField("Email", text: $email )
+    .modifier(EmailFieldStyle())
+    .modifier(TextFieldStyle())
+    .padding([.bottom], 10)
    
-   VStack {
-    Text("Email:").modifier(TextFieldLabelStyle())
-    TextField("Email", text: $email )
-     .modifier(TextFieldStyle())
-   }.padding()
-  
-   VStack {
-    Text("Password:").modifier(TextFieldLabelStyle())
-    SecureField("Password", text: $password )
-     .modifier(TextFieldStyle())
-   }.padding()
+   Text("Password:").modifier(TextFieldLabelStyle())
+   SecureField("Password", text: $password )
+    .disableAutocorrection(true)
+    .modifier(TextFieldStyle())
+    .padding([.bottom], 10)
    
    Button(action: {
     userAuth.login(email: email, password: password)
-  
+    
    }, label: {
     Text("Sign in")
    }).modifier(ButtonStyle())
    
    Spacer()
-   
   }
  }
 }
@@ -221,6 +229,7 @@ struct ResetPassword: View {
  @State var isButtonClicked = false
  @Binding var isResettingPassword: Bool
  @ObservedObject var userAuth = UserAuthentication()
+ @State private var showError = false
  
  var body: some View {
   
@@ -231,11 +240,17 @@ struct ResetPassword: View {
    
    VStack {
     
-    VStack {
-     Text("Email:").modifier(TextFieldLabelStyle())
-      TextField("Email", text: $email )
-       .modifier(TextFieldStyle())
+    if showError {
+     Text("Incorrect email format.")
+      .foregroundColor(.red)
+      .padding()
     }
+    
+    Text("Email:").modifier(TextFieldLabelStyle())
+    TextField("Email", text: $email )
+     .modifier(EmailFieldStyle())
+     .modifier(TextFieldStyle())
+     .padding([.bottom], 10)
     
     if isButtonClicked {
      
@@ -247,8 +262,16 @@ struct ResetPassword: View {
     }
     
     Button(action: {
-     userAuth.resetUserPassword(email: email)
- 
+     Task {
+      do {
+       try await userAuth.resetUserPassword(email: email)
+       print("Password reset email sent successfully")
+      } catch {
+       showError = true
+       print("Error resetting password: \(error.localizedDescription)")
+      }
+     }
+     
     }, label: {
      Text("Send")
      
